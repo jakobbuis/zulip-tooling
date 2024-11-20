2d7e0c498b89398d920e969b3e29392504b21043
Jakob Buis <jakob@jakobbuis.nl>
2024-11-20 13:50:10 +0100

    simplify templating by inlining

--- message.txt.php
+++
@@ -1,8 +0,0 @@
-:wave: Good morning! Today is <?= date('F j, Y'); ?>. It's time for daily standup:
-
-1. What did you accomplish yesterday?
-1. What are you going to finish today?
-1. Are there any blockers in your way?
-1. Clearly describe any out-of-office planned for the next 24 hours.
-
-Please post your answers to the questions below in this thread. We'll start the daily standup with 3 minutes of silent reading. See you in 15 minutes!
--- send.php
+++ send.php
@@ -17,14 +17,22 @@ Dotenv::createImmutable(__DIR__)->load();
 $transport = Transport::fromDsn($_ENV['MAIL_DSN']);
 $mailer = new Mailer($transport);
 
+$date = date('F j, Y');
+$text = <<<EOD
+:wave: Good morning! Today is $date. It's time for daily standup:
+
+1. What did you accomplish yesterday?
+1. What are you going to finish today?
+1. Are there any blockers in your way?
+1. Clearly describe any out-of-office planned for the next 24 hours.
+
+Please post your answers to the questions below in this thread. We'll start the daily standup with 3 minutes of silent reading. See you in 15 minutes!
+EOD;
+
 $email = (new Email())
     ->from(new Address($_ENV['MAIL_FROM_ADDRESS'], $_ENV['MAIL_FROM_NAME']))
     ->to($_ENV['ZULIP_CHANNEL_MAIL'])
-    ->subject($_ENV['ZULIP_TOPIC']);
-
-ob_start();
-echo file_get_contents(__DIR__ . '/message.txt.php');
-$text = ob_get_flush();
-$email = $email->text($text);
+    ->subject($_ENV['ZULIP_TOPIC'])
+    ->text($text);
 
 $mailer->send($email);
