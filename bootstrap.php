273a9000269dd0459ebb252c101734b8f586e234
Jakob Buis <jakob@jakobbuis.nl>
2025-01-15 11:57:27 +0100

    required env vars update

--- bootstrap.php
+++ bootstrap.php
@@ -12,7 +12,10 @@ $dotenv->required([
     'MAIL_FROM_NAME',
     'MAIL_FROM_ADDRESS',
     'ZULIP_CHANNEL_MAIL',
+    'ZULIP_USERNAME',
     'ZULIP_API_KEY',
+    'ZULIP_URL',
+    'ZULIP_STREAM',
 ]);
 
 // Skip operation on national holidays
