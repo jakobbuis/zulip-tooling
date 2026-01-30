f39e99c286420bb580d9c4922d8cb7bf6a1a3cd0
Jakob Buis <jakob@jakobbuis.nl>
2026-01-30 15:14:42 +0100

    fix deprecation notice

--- skip_holidays.php
+++ skip_holidays.php
@@ -5,7 +5,7 @@ use League\Csv\Reader;
 // Skip operation on national holidays
 $path = __DIR__ . '/holidays.csv';
 if (is_readable($path)) {
-    $csv = Reader::createFromPath($path, 'r');
+    $csv = Reader::from($path);
     $csv->setHeaderOffset(0);
     $holidays = array_map(fn ($line) => $line['date'], iterator_to_array($csv->getRecords()));
 
